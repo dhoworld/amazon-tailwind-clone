7141de45c5190e80e73b57ca118b8a6400e541f0
dhoworld <dhoworld@gmail.com>
2021-06-13 17:10:05 -0400

    Added Checkout

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2m1aUvaHx_Zl8Y2BCLoNn6Q8OIEgOR0c",
+  authDomain: "tailwind-clone.firebaseapp.com",
+  projectId: "tailwind-clone",
+  storageBucket: "tailwind-clone.appspot.com",
+  messagingSenderId: "953052818532",
+  appId: "1:953052818532:web:83734c74200bfd72e68371",
+};
+
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+
+export default db;
